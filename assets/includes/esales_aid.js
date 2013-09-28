47d753681bf506ebb35466d353163c794f26aeda
Bishop Lafer <bishoplafer@fullsail.edu>
2013-09-28 14:03:16 -0400

    Clickable Logo
    Dynamic Page Detection

--- assets/includes/esales_aid.js
+++ assets/includes/esales_aid.js
@@ -5,13 +5,17 @@ $(document).ready(function(){
 
 	setOrientationListener();
 	
-	changePage('home.html?v=1');
-	
 	$('nav a').on('click', function(){
 		$('nav a').removeClass('selected');
 		$(this).addClass('selected');
-		changePage($(this).attr('data-file'));
-	})
+		changePage( $(this).attr('data-file') );
+	});
+	
+	$('nav a:nth-child(1)').trigger('click');
+	
+	$('.banner_logo').on('click', function(){
+		$('nav a:nth-child(1)').trigger('click');
+	});
 
 });
 
@@ -19,7 +23,14 @@ function changePage(fileName){
 	$('.content_container').animate({opacity:0}, 500, function(){
 		$('.content_loading_container').load('assets/content/'+fileName, function(){
 			$('.content_container').delay(250).animate({opacity:1}, 500);
-		})
+		});
+		
+		if( fileName == 'home.html?v=1'){
+			$('page').addClass('home');
+		}else{
+			$('.page').removeClass('home');
+		}
+		
 	});
 }
 
