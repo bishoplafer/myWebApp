983d495c2ae9b2d9c56c282b3cddb8bf5e5b588d
Bishop Lafer <bishoplafer@fullsail.edu>
2013-09-27 22:32:20 -0400

    Create an iPad Web App
    Lynda.com tutorial excercise created by Chris Converse

---
+++ assets/includes/esales_aid.js
@@ -0,0 +1,35 @@
+/*  JavaScript Document                      */
+
+
+$(document).ready(function(){
+
+	setOrientationListener();
+	
+	changePage('home.html?v=1');
+	
+	$('nav a').on('click', function(){
+		$('nav a').removeClass('selected');
+		$(this).addClass('selected');
+	})
+
+});
+
+function setOrientationListener(){
+	rotationInterval = setInterval( function(){ updateOrientation(); }, 500 );
+}
+
+function updateOrientation(){
+	if($('body').width() < 1024){
+		$('.page').removeClass('landscape').addClass('portrait');
+	}else{
+		$('.page').removeClass('portrait').addClass('landscape');	
+	}
+}
+
+function changePage(fileName){
+	$('.content_container').animate({opacity:0}, 500, function(){
+		$('.content_loading_container').load('assets/content/'+fileName, function(){
+			$('.content_container').delay(250).animate({opacity:1}, 500);
+		})
+	});
+}
